df5891b4f876bc28a57a37840643b3fbd11391f7
Manuel Venegas <mengasis@outlook.com>
2018-07-11 15:41:48 -0400

    Added eslint config and others dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+	"extends": "eslint:recommended",
+	"parser": "babel-eslint",	
+    "parserOptions": {
+        "sourceType": "module"
+	},
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};

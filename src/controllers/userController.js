a1eb8e41a964a01c436b9c6937d495283e0a81e6
Manuel Venegas <mengasis@outlook.com>
2018-07-23 23:10:19 -0400

    [Fix] Enhanced controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,11 +5,7 @@ export const getUser = async (req, res) => {
 
 	const user = await findUser({ userId })
 
-	if (!user)
-		res.status(404)
-			.send({ message: 'User not found.' })
-			.end()
-	res.status(200)
-		.send(user)
-		.end(true)
+	if(!user) res.status(404).end()
+
+	res.status(200).send(user)
 }

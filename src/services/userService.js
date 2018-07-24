c5fcd8579cf5e11b7880c637b4b0951679bf5e6e
Manuel Venegas <mengasis@outlook.com>
2018-07-23 23:48:50 -0400

    Added an alternative for raw queries for Sequelize

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,3 +3,9 @@ import UserModel from '../models/User'
 
 export const findUser = params => UserModel.findOne({ where: params, raw: true })
 
+
+export const findAllUserWithQuery = () => db.query('SELECT * from user WHERE 1' , {
+	replacements: {
+	},type: db.QueryTypes.SELECT
+})
+
